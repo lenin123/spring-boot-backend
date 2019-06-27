33a2de20c70f8d7e360393e0dc0d716af98bc40a
Lênin Müller <lenin_muller@uol.com.br>
2019-06-27 09:07:15 -0300

    Correções

--- src/main/java/com/leninmuller/cursomc/services/CategoriaService.java
+++ src/main/java/com/leninmuller/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.leninmuller.cursomc.domain.Categoria;
+import com.leninmuller.cursomc.domain.Cliente;
 import com.leninmuller.cursomc.dto.CategoriaDTO;
 import com.leninmuller.cursomc.repositories.CategoriaRepository;
 import com.leninmuller.cursomc.services.exceptions.DataIntegrityException;
@@ -33,8 +34,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateDate(newObj, obj);
+		return repo.save(newObj);
 	}	
 	
 	public void delete(Integer id) {
@@ -58,4 +60,8 @@ public class CategoriaService {
 	public Categoria findDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
+	
+	private void updateDate(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}	
 }
