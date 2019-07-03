29583040caeeb5373f4696b8e6d02ea11713c469
Lênin Müller <lenin_muller@uol.com.br>
2019-07-03 14:17:29 -0300

    Implementando toString de Pedido

--- src/main/java/com/leninmuller/cursomc/domain/Pedido.java
+++ src/main/java/com/leninmuller/cursomc/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.leninmuller.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -135,6 +138,28 @@ public class Pedido implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");		
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido Número: ");		
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));		
+		builder.append(", Cliente: ");
+		builder.append(getCliente());		
+		builder.append(", Situação do Pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());		
+		builder.append("\nDetalhes:\n");
+		for(ItemPedido itemsPedido : getItemsPedido()) {
+			builder.append(itemsPedido.toString());	
+		}
+		builder.append("Valor Total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
 	}
 
 
--- src/main/java/com/leninmuller/cursomc/services/PedidoService.java
+++ src/main/java/com/leninmuller/cursomc/services/PedidoService.java
@@ -33,6 +33,9 @@ public class PedidoService {
 
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 
 	
 	public Pedido find(Integer id) {  
@@ -45,6 +48,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -55,10 +59,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for(ItemPedido itemPedido : obj.getItemsPedido()) {
 			itemPedido.setDesconto(0.0);
-			itemPedido.setPreco(produtoService.find(itemPedido.getProduto().getId()).getPreco());
+			itemPedido.setProduto(produtoService.find(itemPedido.getProduto().getId()));
+			itemPedido.setPreco(itemPedido.getProduto().getPreco());
 			itemPedido.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItemsPedido());
+		System.out.println(obj);
 		return obj;
 	}
 		
